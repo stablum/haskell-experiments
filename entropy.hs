c70ce9a8ee0021619710abffaccd9024e3d32111
Francesco Stablum <stablum@gmail.com>
2014-11-15 20:18:45 +0100

    entropy

---
+++ entropy.hs
@@ -0,0 +1,2 @@
+freq2prob l = [ (curr/ sum(l)) | curr <- l ]
+entropy p = - (sum [ (curr * (log curr)) | curr <- p])
---
+++ grapefruit_test.hs
@@ -0,0 +1,4 @@
+import Graphics.UI.Grapefruit.Circuit
+import Graphics.UI.Grapefruit.UIBackend
+import Examples.Grapefruit.Example
+run UIBackend mainCircuit ()
